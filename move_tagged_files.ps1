14d64964892ac0dd7174edae823ac125025fceff
Ragefriar <39865619+Ragefriar@users.noreply.github.com>
2018-06-04 22:21:01 +0100

    Path changes for MacPro

--- move_tagged_files.ps1
+++ move_tagged_files.ps1
@@ -1,8 +1,7 @@
 # Set global variables
-$done = "/mnt/nas3-archive/Downloads/done"
-$staging = "/mnt/nas4-tv/staging"
-$nas4tv_adult = "/mnt/nas4-tv/TV/TV - Adult"
-$nas4tv_kids = "/mnt/nas4-tv/TV/TV - Kids"
+$done = "/Volumes/Data/Downloads/done"
+$tv_adult = "/Volumes/Data/TV/TV - Adult"
+$tv_kids = "/Volumes/Data/TV/TV - Kids"
 $adult_tv = @(
     "American Dad!"
     "American Gods"
@@ -10,40 +9,40 @@ $adult_tv = @(
     "Black Mirror"
     "Blindspot"
     "Britannia"
+    "Brooklyn Nine-Nine"
     "Colony"
     "Dark Matter"
-    "DC\'s Legends of Tomorrow"
-    "Doctor Who \(2005\)"
+    "DC's Legends of Tomorrow"
+    "Doctor Who (2005)"
     "Family Guy"
     "Game of Thrones"
     "Happy"
     "Impractical Jokers"
     "iZombie"
     "Legion"
-    "Lost in Space \(2018\)"
+    "Lost in Space (2018)"
     "Lucifer"
-    "Marvel\'s Daredevil"
-    "Marvel\'s Iron Fist"
-    "Marvel\'s Jessica Jones"
-    "Marvel\'s Luke Cage"
-    "Marvel\'s The Defenders"
-    "Marvel\'s The Punisher"
+    "Marvel's Daredevil"
+    "Marvel's Iron Fist"
+    "Marvel's Jessica Jones"
+    "Marvel's Luke Cage"
+    "Marvel's The Defenders"
+    "Marvel's The Punisher"
     "Midnight, Texas"
     "MINDHUNTER"
     "Mom"
     "Mr. Pickles"
-    "New Girl"
     "Our Girl"
     "Over There"
     "Peaky Blinders"
-    "Peter Kay\'s Car Share"
+    "Peter Kay's Car Share"
     "Plebs"
     "Preacher"
     "Rick and Morty"
     "Riverdale"
     "Scream"
     "SEAL Team"
-    "Six"
+    "SIX"
     "South Park"
     "SS-GB"
     "Star Trek Discovery"
@@ -53,8 +52,8 @@ $adult_tv = @(
     "The Detour"
     "The End of the Fxxxing World"
     "The Expanse"
-    "The Flash \(2014\)"
-    "The Goldbergs \(2013\)"
+    "The Flash (2014)"
+    "The Goldbergs (2013)"
     "The Good Doctor"
     "The Handmaid's Tale"
     "The Long Road Home"
@@ -62,31 +61,27 @@ $adult_tv = @(
     "The New Legends of Monkey"
     "The Orville"
     "The Rain"
-    "The Tick \(2016\)"
+    "The Tick (2016)"
     "The Walking Dead"
     "The X-Files"
-    "Timeless \(2016\)"
+    "Timeless (2016)"
     "Westworld"
     "Wolf Creek"
     "Wynonna Earp"
 )
 $kids_tv = @(
-    "Danger Mouse \(2015\)"
     "Dragons"
     "Henry Danger"
-    "Lab Rats \(2012\)"
     "My Little Pony"
     "Steven Universe"
-    "Teenage Mutant Ninja Turtles \(2012\)"
+    "Teenage Mutant Ninja Turtles (2012 )"
     "The Amazing World of Gumball"
     "The Simpsons"
     "The Thundermans"
+    "Trollhunters"
 )
 $adult_tv = $adult_tv | Sort-Object
 $kids_tv = $kids_tv | Sort-Object
-
-# Set permissions on files
-&chmod 777 $done/*
 
 # Rename & move Adult TV
 ForEach ($program in $adult_tv){
@@ -95,11 +90,9 @@ ForEach ($program in $adult_tv){
         If ($tv -cmatch $program){
             $short_name = $tv.Name -replace "$program - ", "" 
             Rename-Item -path $tv.FullName -NewName $short_name
-            $program_name = $program -replace [regex]::Escape('\'),""
-            $naspath = "$nas4tv_adult" + "/" + "$program_name"
-            &rsync -P -e ssh "$done/$short_name" "root@nas4.ragefire.local:'$staging'"
-            ssh root@nas4.ragefire.local "mv /mnt/nas4-tv/staging/*.m4v '$naspath'"
-#            Remove-Item -path "$done/$short_name"
+            $macpath = "$tv_adult" + "/" + "$program"
+            Move-Item -path "$done/$short_name" -destination "$macpath"
+            # Remove-Item -path "$done/$short_name"
         }
     }
 }
@@ -111,11 +104,9 @@ ForEach ($program in $kids_tv){
         If ($tv -cmatch $program){
             $short_name = $tv.Name -replace "$program - ", "" 
             Rename-Item -path $tv.FullName -NewName $short_name
-            $program_name = $program -replace [regex]::Escape('\'),""
-            $naspath = "$nas4tv_kids" + "/" + "$program_name" 
-            &rsync -P -e ssh "$done/$short_name" "root@nas4.ragefire.local:'$staging'"
-            ssh root@nas4.ragefire.local "mv /mnt/nas4-tv/staging/*.m4v '$naspath'"
-#            Remove-Item -path "$done/$short_name"
+            $macpath = "$tv_kids" + "/" + "$program"
+            Move-Item -path "$done/$short_name" -destination "$macpath"
+            # Remove-Item -path "$done/$short_name"
         }
     }
 }
